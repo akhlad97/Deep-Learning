46ed7276e6f2cf62121385a1925538063ba729e6
akhlad97 <146697448+akhlad97@users.noreply.github.com>
2025-10-05 12:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import tensorflow
+import tensorflow as st
 from sklearn.preprocessing import StandardScaler ,OneHotEncoder,LabelEncoder
 import pandas as pd
 import pickle
